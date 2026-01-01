598cc1cb2f14d5e533c725c647bf836a74ca4263
protolambda <proto@protolambda.com>
2026-01-01 19:22:06 +0100

    use atomic int for safe dynamic log-level changes

--- log/level_handler.go
+++ log/level_handler.go
@@ -3,18 +3,21 @@ package log
 import (
 	"context"
 	"log/slog"
+	"sync/atomic"
 )
 
 type LevelHandler struct {
 	inner slog.Handler
-	lvl   slog.Level
+	lvl   atomic.Int64 // slog.Level
 }
 
 var _ Handler = (*LevelHandler)(nil)
 
 func LevelMod(minLvl slog.Level) HandlerMod {
 	return func(h slog.Handler) slog.Handler {
-		return &LevelHandler{inner: h, lvl: minLvl}
+		out := &LevelHandler{inner: h}
+		out.SetMinLevel(minLvl)
+		return out
 	}
 }
 
@@ -23,7 +26,7 @@ func (h *LevelHandler) Unwrap() slog.Handler {
 }
 
 func (h *LevelHandler) Enabled(ctx context.Context, lvl slog.Level) bool {
-	if lvl < h.lvl {
+	if lvl < h.MinLevel() {
 		return false
 	}
 	return h.inner.Enabled(ctx, lvl)
@@ -34,23 +37,25 @@ func (h *LevelHandler) Handle(ctx context.Context, r slog.Record) error {
 }
 
 func (h *LevelHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return &LevelHandler{
+	out := &LevelHandler{
 		inner: h.inner.WithAttrs(attrs),
-		lvl:   h.lvl,
 	}
+	out.SetMinLevel(h.MinLevel())
+	return out
 }
 
 func (h *LevelHandler) WithGroup(name string) slog.Handler {
-	return &LevelHandler{
+	out := &LevelHandler{
 		inner: h.inner.WithGroup(name),
-		lvl:   h.lvl,
 	}
+	out.SetMinLevel(h.MinLevel())
+	return out
 }
 
 func (h *LevelHandler) MinLevel() slog.Level {
-	return h.lvl
+	return slog.Level(h.lvl.Load())
 }
 
 func (h *LevelHandler) SetMinLevel(lvl slog.Level) {
-	h.lvl = lvl
+	h.lvl.Store(int64(lvl))
 }
